57182f4da40f5944b2387651713d0b272745af04
saurabhyadav005 <sy7408694041@gmail.com>
2026-07-02 12:36:45 +0530

    Module 2 Complete

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// Welcome Message
-console.log("Welcome to Saurabh Yadav Portfolio Website");
-
-// Contact Form Validation
-const form = document.querySelector("form");
-
-if (form) {
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const name = document.getElementById("name").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const message = document.getElementById("message").value.trim();
-
-        if (name === "" || email === "" || message === "") {
-            alert("Please fill all fields.");
-            return;
-        }
-
-        alert("Thank you! Your message has been submitted.");
-
-        form.reset();
-    });
+// Dark Mode Toggle
+function toggleTheme() {
+    document.body.classList.toggle("dark");
 }
+
+// Active Navigation
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(item => item.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        ?.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
